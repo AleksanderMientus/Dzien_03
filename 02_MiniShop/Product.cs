3254dfbaa94eb9ed3c7a5c81b3323027916524cd
Aleksander Mientus <aleksander.mientus@synevo.pl>
2021-07-02 11:21:48 +0200

    Aktualizacja

---
+++ 02_MiniShop/Product.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_MiniShop
+{
+    /// <summary>
+    /// Klasa opisująca produkt jednostkowy
+    /// </summary>
+    class Product
+    {
+        private int id;
+        private string name;    // NULL - deklarowany string bez wartości
+        private double price;
+        private string descr = string.Empty;   // description - opis // string.Empty - ""
+        private bool promo = false;
+        private bool active = true;
+
+        // właściwość (Z wielkiej litery), pola (z małej litery)
+        // wystawianie wartości na zewnątrz bez robienia metody i bez możliwości zmiany z zewnątrz
+        // gettery
+        public bool Active 
+        {
+            get { return active; }
+        }
+        public double Price
+        {
+            get { return price; }
+        }
+
+        public Product(int id, string name, double price, string descr = "")
+        {
+            this.id = id; this.name = name; this.price = price; this.descr = descr;
+        }
+
+        public void SetActive(bool active)
+        {
+            this.active = active;
+        }
+
+        public bool GetActive()
+        {
+            return active;
+        }
+
+    }
+}
